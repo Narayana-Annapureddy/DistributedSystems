0d7ad840e139da759a59e66b100fa41ac5c5c166
karthik9100 <karthikshiruri@gmail.com>
2024-01-21 15:24:12 +0530

    added basic server with two endpoints

---
+++ Server/App.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import jsonify, request
+import os
+app = Flask(__name__)
+
+@app.route("/",methods = ["GET"])
+@app.route("/home",methods = ["GET"])
+def hello_world():
+    
+    server_id = os.environ.get('server_id')
+    no_of_servers = os.environ["no_of_servers"]
+    msg = {
+        "message": f"Hello, From Server{server_id}",
+        "no_of_servers" : no_of_servers,
+        "status" : "Successful"
+    }
+    return jsonify(msg),200
+
+@app.errorhandler(404)
+
+def errorPage(k):
+    return "Page not found"
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0",port=5000 , debug = True)
