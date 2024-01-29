3c8603a92662677320227100f15dd893b761f694
swathijanapala <76417826+swathijanapala@users.noreply.github.com>
2024-01-30 01:48:51 +0530

    Update requests.py
    This program is to send 10,000 client requests to the server to see how load got distributed among n-servers.

--- requests.py
+++ requests.py
@@ -13,6 +13,14 @@ async def main():
         tasks = [make_request(session, url) for _ in range(10000)]
         responses = await asyncio.gather(*tasks)
         
+    for res in responses:
+            server_ids.append(json.loads(res)["message"].split()[-1])
+
+    
+    with open('load_distribution.txt', 'a') as f:
+        for i in server_ids:
+            f.write(i[-1] + "\n")
+        
         
 
 
@@ -20,19 +28,20 @@ if __name__ == "__main__":
     asyncio.run(main())
 
     '''
-    #Bar chart
+    # For bar chart
     file_path = 'load_distribution.txt'
-    dictionary={'0':0,'1':0,'2':0}
+    dictionary={'1':0,'2':0,'3':0}
     with open(file_path,'r') as file:
         for line in file:
             line1=line.strip()
             dictionary[line1]=dictionary[line1]+1
 
-    servers=['Server-0','Server-1','Server-2']
+    servers=['Server-1','Server-2','Server-3']
     plt.bar(servers,list(dictionary.values()))
     plt.xlabel('servers')
     plt.ylabel('Distribution')
     plt.title('Load Distribution among 3 severs upon 10,000 asyn requests')
+    plt.savefig('bar3.png')
     plt.show()
     '''
     
